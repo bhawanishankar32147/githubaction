9bbaab8f462db8b0493b8c0ad1fac5cbea716836
Bhabs <bhawanishankar321@gmail.com>
2025-07-27 19:30:40 +0530

    some correctrions were made

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,13 @@
-from app.model import train_and_save_model
+import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 def test_accuracy():
-    acc = train_and_save_model()
+    iris = load_iris()
+    _, X_test, _, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=42)
+    # Load the saved model
+    clf = joblib.load("app/iris.json")
+    acc = accuracy_score(y_test, clf.predict(X_test))
     assert acc > 0.85, f"Accuracy too low: {acc}"
